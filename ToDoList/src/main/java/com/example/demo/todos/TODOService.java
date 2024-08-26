399cc323cfbe234bc091208332264505c7c1d402
Rashidat <rawsheedah@yahoo.com>
2024-08-26 13:59:02 +0100

    Add TODOService class

--- ToDoList/src/main/java/com/example/demo/todos/TODOService.java
+++ ToDoList/src/main/java/com/example/demo/todos/TODOService.java
@@ -1,3 +1,39 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
 public class TODOService {
-    
+    // Implements the (TODORepository) logic
+
+    @Autowired // pulls in the repository
+    private TODORepository todoRepository;
+
+    public TODOService(TODORepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+
+    // Get all TODOs
+    public List<TODO> getAllTODOs() {
+        return this.todoRepository.findAll();
+    }
+
+    // Get a todo by its id
+    public TODO getTODOById(Long id) throws NoSuchElementException {
+        return this.todoRepository.findById(id).orElseThrow();
+    }
+
+    // Create and save a new or existing todo
+    public TODO createTODO(TODO todo) throws IllegalArgumentException, OptimisticLockingFailureException {
+        return this.todoRepository.save(todo);
+    }
+
+    // Update a todo by its id
+
 }
