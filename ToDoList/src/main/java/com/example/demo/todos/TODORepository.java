a7c825d631fb63792b74c61373cf2a38f05810cb
Rashidat <rawsheedah@yahoo.com>
2024-08-26 10:13:34 +0100

    Add TODORepository interface

--- ToDoList/src/main/java/com/example/demo/todos/TODORepository.java
+++ ToDoList/src/main/java/com/example/demo/todos/TODORepository.java
@@ -1,3 +1,13 @@
-public class TODORepository {
-    
+package com.example.demo;
+
+
+import org.springframework.data.repository.ListCrudRepository;
+
+
+/** 
+ * Interface that extends ListCrudRepository:
+ * Access logic from service, and interact with the database,
+ * using ListCrudRepository which returns a list.
+ */ 
+public interface TODORepository extends ListCrudRepository<TODO, Long> {
 }
