e58d3ff3fa397e05e33291a3ab11fa73f1dede2e
Rashidat <rawsheedah@yahoo.com>
2024-08-26 09:33:31 +0100

    Add the Model: TODO class for Entity, Table, constructors with the getters and setters

--- ToDoList/src/main/java/com/example/demo/todos/TODO.java
+++ ToDoList/src/main/java/com/example/demo/todos/TODO.java
@@ -1,4 +1,62 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+ // Model that speaks to the database
+@Entity
+@Table(name = "todos")
+
 public class TODO {
-    // Model that speaks to the database
+
+    @Id
+    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean taskCompleted;
+
+    // constructors
+    public TODO() {
+    }
+
+    public TODO(Long id, String title, String description, boolean taskCompleted) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.taskCompleted = taskCompleted;
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
+    public boolean getTaskCompleted() {
+        return taskCompleted;
+    }
+
+    public void setTaskCompleted(boolean taskCompleted) {
+        this.taskCompleted = taskCompleted;
+    }
 }
